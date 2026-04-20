01bfe9808143ab60a5fc1b7e8a188dc33ef66052
Autumn May Valley <draqoshaimposter@gmail.com>
2026-04-20 17:14:53 +0300

    Fix input parsing and update output statements

--- homework/2.cs
+++ homework/2.cs
@@ -1,15 +1,15 @@
-namespace epstein_files_csharp_edition;
+namespace ConsoleApp1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int number1 = Console.ReadLine();
-        int number2 = Console.ReadLine();
-        int number3 = Console.ReadLine();
-        int number4 = Console.ReadLine();
-        int number5 = Console.ReadLine();
-        int list[] = { number1, number2, number3, number4, number5 };
+        int number1 = int.Parse(Console.ReadLine());
+        int number2 = int.Parse(Console.ReadLine());
+        int number3 = int.Parse(Console.ReadLine());
+        int number4 = int.Parse(Console.ReadLine());
+        int number5 = int.Parse(Console.ReadLine());
+        int[] list = { number1, number2, number3, number4, number5 };
         Console.WriteLine($"Sum: {number1 + number2 + number3 + number4 + number5}");
         int max = 0;
         foreach (int number in list)
@@ -27,6 +27,8 @@ class Program
                 min = number;
             }
         }
-        cout << $"Max: {max}\nMin: {min}";
-        cout << $"The full multiplication product is: {number1 * number2 * number3 * number4 * number5}";
+        Console.WriteLine($"Max: {max}");
+        Console.WriteLine($"Min: {min}");
+        Console.WriteLine($"Result of the multiplication of the numbers: {number1 * number2 * number3 * number4 * number5}");
     }
+}
