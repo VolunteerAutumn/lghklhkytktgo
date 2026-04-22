d87bfc731cf3bce2547d477d9de5e283c4155c49
Autumn May Valley <draqoshaimposter@gmail.com>
2026-04-22 19:02:16 +0300

    Add initial Program class with main method

---
+++ homework/1.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("It's easy to win forgiveness for being wrong; being right is what gets you\ninto real trouble.");
+        Console.WriteLine("\t\t\tBjarne Stroustrup");
+    }
+}
